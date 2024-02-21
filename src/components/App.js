d9b60a65dd2e7d09d14da414596c1013b1e3d1f2
Soojungkim <soojung.kimm@gmail.com>
2024-02-21 10:02:21 -0800

    working on bonus

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import DarkModeWrapper from "./DarkModeWrapper";
 import Header from "./Header";
 import TweetsContainer from "./TweetsContainer";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode((currentValue) => !currentValue);
+  };
   return (
-    <DarkModeWrapper darkMode={false}>
-      <Header />
+    <DarkModeWrapper darkMode={darkMode}>
+      <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <TweetsContainer />
     </DarkModeWrapper>
   );
--- src/components/TweetsContainer.js
+++ src/components/TweetsContainer.js
@@ -5,6 +5,11 @@ import { users as userData } from "../data/data";
 
 function TweetsContainer() {
   const [users, setUsers] = useState(userData);
+  const [currentTweet, setCurrentTweet] = useState(0);
+
+  const handleUserClick = (userId) => {
+    setCurrentTweet(userId - 1);
+  };
 
   console.log("In TweetsContainer, state is", users);
   return (
@@ -12,11 +17,11 @@ function TweetsContainer() {
       <div className="ui grid">
         <div className="six wide column">
           <h2 className="ui header">Users</h2>
-          <UserList users={users} />
+          <UserList users={users} handleUserClick={handleUserClick} />
         </div>
         <div className="ten wide column">
           <h2 className="ui header">Tweets</h2>
-          <TweetList user={users[0]} />
+          <TweetList user={users[currentTweet]} />
         </div>
       </div>
     </div>
